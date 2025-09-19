a10d58b464420c8a2f07ea30268a23850295aa12
Ogunnubi <ogunnubioluwaseun01@gmail.com>
2025-09-20 00:42:46 +0100

    navbar changes

--- src/components/navbar/ImgLogo.tsx
+++ src/components/navbar/ImgLogo.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
-import ImgLogo from "../../../public/logo.png"; import styles from "./ImgLogo.module.scss";
+import ImgLogo from "../../../public/text-img-logo.png";
+import styles from "./ImgLogo.module.scss";
 import Link from "next/link";
 
 export default function Logo() {
   return (
     <Link href="/" className={styles.logo}>
       <Image src={ImgLogo}  alt="The Wealthy Post Logo"
-        width={180}
-        height={50}
         priority
         sizes="(max-width: 768px) 140px, 180px"
-        className=" bg-[#ffffff] rounded"
+        className=" bg-[#ffffff] rounded color-[#000000]"
       />
     </Link>
   );
